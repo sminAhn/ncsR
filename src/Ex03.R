c250d97802454fe59144702f67e8df22be6dc1b2
sminAhn <50534262+sminAhn@users.noreply.github.com>
2020-04-28 23:19:43 +0900

    Update Ex03.R

--- src/Ex03.R
+++ src/Ex03.R
@@ -1,22 +1,13 @@
-install.packages("corrplot")
+#  주성분 분석
 
-library(corrplot)
+data(iris) # iris 데이터를 사용하기 위한 코드
 
-CorrEuStockMarkets <- cor(EuStockMarkets)
+iris.pca <- prcomp(iris[,1:4]) # iris데이터 중 1~4칼럼의 데이터로 PCA수행
+iris.pca # 주성분분석 결과 출력
+summary(iris.pca) # PCA 결과 요약
 
-corrplot(CorrEuStockMarkets, method="ellipse")
+iris.predict <- predict(iris.pca) # 주성분 점수 계산
+iris.predict[, 1:2] # 주성분 1과 주성분 2의 점수 출력
+biplot(iris.pca)    # 주성분 1과 주성분 2를 이용한 산점도 출력
 
 
-data("iris")
-
-iris.pca <- prcomp(iris[,1:4])
-
-iris.pca
-
-summary(iris.pca)
-
-iris.predict <- predict(iris.pca)
-
-iris.predict[,1:2]
-
-biplot(iris.pca)
